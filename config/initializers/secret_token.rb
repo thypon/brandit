98ac61f7bada17dc313b1f7cf58b9cdd8fe959a9
Andrea Brancaleoni <null@gmx.it>
2013-06-05 11:48:03 +0200

    INITIAL COMMIT

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Brandit::Application.config.secret_token = '55b17d2d1d5c1059dd64c17629f95c4e1e4f2d085de937214dc05777a7c5afcfb11d9a35b712504e4706e317b65517ef2fa155f0dd04444ff81b28898e9add8f'
