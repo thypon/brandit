acd0addd474423321fe211ac85021959b134ee7e
Andrea Brancaleoni <null@gmx.it>
2013-06-05 17:11:23 +0200

    Added Product Photo

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,10 +1,12 @@
 class Product < ActiveRecord::Base
   belongs_to :product_type
   belongs_to :product_style
-  attr_accessible :description, :name
+  attr_accessible :description, :name, :photo
 
   has_and_belongs_to_many :designers
   has_and_belongs_to_many :services
   has_and_belongs_to_many :showrooms
   has_and_belongs_to_many :shops
+
+  has_attached_file :photo
 end
