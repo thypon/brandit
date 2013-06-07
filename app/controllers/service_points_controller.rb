23a02e6585ee54cdd7794ba65e74bc5d2f45bd50
Lorenzo Affetti <affox90@gmail.com>
2013-06-07 19:06:12 +0200

    service points controller: some fixing

--- app/controllers/service_points_controller.rb
+++ app/controllers/service_points_controller.rb
@@ -1,6 +1,6 @@
 class ServicePointsController < ApplicationController
   def index
-  	if id = params[:city_id]
+  	if id = params[:service_point_city_id]
   		city = City.find(id)
   		@service_points = city.service_points
   		@title = city.name
@@ -19,7 +19,7 @@ class ServicePointsController < ApplicationController
   def next
   	service_point = ServicePoint.find(params[:id])
 
-  	if id = params[:city_id]
+  	if id = params[:service_point_city_id]
   		city = City.find(id)
   		service_points = city.service_points
   		next_index = service_points.index(service_point) + 1
@@ -35,7 +35,7 @@ class ServicePointsController < ApplicationController
   def previous
   	service_point = ServicePoint.find(params[:id])
 
-  	if id = params[:city_id]
+  	if id = params[:service_point_city_id]
   		city = City.find(id)
   		service_points = city.service_points
   		previous_index = service_points.index(service_point) - 1
@@ -49,7 +49,7 @@ class ServicePointsController < ApplicationController
   end
 
   def up
-  	if params[:city_id]
+  	if params[:service_point_city_id]
   		city = City.find(id)
   		redirect_to service_point_city_service_points_path(city)
   	elsif id = params[:service_id]
